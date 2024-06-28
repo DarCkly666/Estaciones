be69ef40d4f8cc452ec2e73780d831709eb100cd
DarCkly666 <darckly6@--->
2024-06-28 17:58:12 -0400

    First commit

---
+++ estaciones.api/src/test/java/com/darckly/estaciones/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darckly.estaciones.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
